3d1722ac9fe5391b037f5e1dd73792c97ddc854d
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2022-12-13 02:23:40 -0600

    Grid 99% done
    Got grid made still need numbers and letters on z and y axis

--- part-1.js
+++ part-1.js
@@ -1,20 +1,22 @@
 var rLS = require('readline-sync');
 
-// var keyPress = rLS.keyIn('Press any key to start the game! ');
+var keyPress = rLS.keyIn('Press any key to start the game!');
 
-
+const shipSize = 1;
 let size = 3;
 let board = "";
 
 for (let y = 0; y < size; y++) {
   for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) { 
+    if (x + y) { 
       board += "-";
     } else {
-      board += "+";
+      board += "-";
     }
   }
   board += "\n";
+  
 }
-
 console.log(board);
+
+
