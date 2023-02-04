da593f43f67d6da6c0bd1081ae5bd845dedccee6
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-02-04 04:14:38 -0600

    Started over
    went back to drawing board started with  a function to create a grid
    I have a rough idea

--- part-1.js
+++ part-1.js
@@ -1,28 +1,11 @@
-var rLS = require('readline-sync');
-
-// var start = rLS.keyIn('Press any key to start the game! ');
-
-const board = {
-  a: ['1', '2', '3'],
-  b: ['4', '5', '6'],
-  c: ['7', '8', '9'],
-};
-
-const cell = Object.entries(board);
-
-function sample(arr) {
-  let rowPick = arr[Math.floor(Math.random() * arr.length)];
-  let colPick = rowPick[0] + rowPick[1][Math.floor(Math.random() * rowPick[1].length)];
-  return colPick;
+const rLS = require("readline-sync");
+// var start = rLS.keyIn("Press any key to start the game! ");
+function createBoard(size) {
+	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	const x = nums.slice(0, size);
+	const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+	const y = letters.slice(0, size);
+	return `${x}\n${y}`;
 }
 
-function sample2(arr) {
-  let rowPick2 = arr[Math.floor(Math.random() * arr.length)];
-  let colPick2 = rowPick2[0] + rowPick2[1][Math.floor(Math.random() * rowPick2[1].length)];
-  return colPick2;
-}
-
-let ship1 = sample(cell);
-let ship2 = sample2(cell);
-
-console.log(ship1, ship2);
+console.log(createBoard(3));
