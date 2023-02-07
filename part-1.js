4ce9498d01a49d90c6ba5b01ddef4ec543b78d52
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-02-07 14:58:06 -0600

    Made new function to create board
    Trying to find a way to place ships on grid.

--- part-1.js
+++ part-1.js
@@ -1,11 +1,28 @@
 const rLS = require("readline-sync");
 // var start = rLS.keyIn("Press any key to start the game! ");
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+const topBorder = "+-";
+let sideBorder = "| ";
+let ship1 = "|x";
+let ship2 = "|x";
 function createBoard(size) {
-	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	const x = nums.slice(0, size);
-	const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-	const y = letters.slice(0, size);
-	return `${x}\n${y}`;
+	const xTopAndBottom = topBorder.repeat(size);
+	function createNums(column) {
+		const x = nums.slice(0, column);
+		let xNum = "";
+		xNum += `${x}\n ${xTopAndBottom}+`;
+		return xNum;
+	}
+	const sides = sideBorder.repeat(size + 1);
+	function createLetters(letterLength) {
+		const y = letters.slice(0, letterLength);
+		let yLetter = "";
+		for (let i = 0; i < y.length; i++) {
+			yLetter += `${y[i] + sides}\n ${xTopAndBottom}+\n`;
+		}
+		return yLetter;
+	}
+	return `  ${createNums(size)}\n${createLetters(size)}`;
 }
-
 console.log(createBoard(3));
