353494203057160501f66d557ba29d590683dd48
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-02-16 02:38:37 -0600

    Bugs corrected and new bugs found.
    Handled Already guessed locations. Fixed console logs to reflect
    the correct the amount of ships sunk. If one is sunk say one ship
    remaining, if both ships are sunk say both ships are sunk run play
    again sequence.
    
    New bugs. Input strike position must be in a lowercase format other
    wise console prints Not a valid poistion.
    If position is undefined the next entry no matter valid or not,
    code will break.

--- part-1.js
+++ part-1.js
@@ -33,16 +33,17 @@ function game() {
 	console.log(shipA, shipB);
 	let checkStrike = [];
 	let sunkShips = [];
+	let invalidPosition = [];
 	function strikeCall() {
 		let strike = rls.question("Please enter a location to strike: ");
 		if (strike[0] === undefined) {
+			invalidPosition.push(strike);
 			console.log("No location given. Please try again.");
 			strikeCall();
 		}
 		let [letter, number] = strike;
 
 		if (letters.includes(letter.toLowerCase()) && nums.includes(number)) {
-			checkStrike.push(letter + number);
 			for (let i = 0; i < checkStrike.length; i++) {
 				if (letter + number === checkStrike[i]) {
 					console.log(
@@ -51,34 +52,42 @@ function game() {
 					strikeCall();
 				}
 			}
+			checkStrike.push(letter + number);
 		} else {
 			console.log("Invalid location. Please try again.");
 			strikeCall();
 		}
 	}
 	strikeCall();
+	function checkSunkShips() {
+		if (sunkShips.length === 1) {
+			console.log("You sunk my BattleShip! 1 ship remaining!!");
+		} else if (sunkShips.length === 2) {
+			console.log("You've sunk all my Battleships!!");
+			let playAgain = rls.keyInYN("Would you like to play again?");
+			if (playAgain === true) {
+				checkStrike.length = 0;
+				sunkShips.length = 0;
+				game();
+			} else {
+				console.log("Have a good Day.");
+				process.exit();
+			}
+		}
+	}
 	function findHits() {
 		for (let position of checkStrike) {
-			if (shipA[0] === position || shipB[0] === position) {
+			if (shipA[0] === position) {
 				sunkShips.push(shipA[0]);
+				checkSunkShips();
+				strikeCall();
+			} else if (shipB[0] === position) {
 				sunkShips.push(shipB[0]);
-				console.log("You sunk my BattleShip! 1 ship remaining!!");
+				checkSunkShips();
 				strikeCall();
 			} else {
 				console.log("You missed! Please try again.");
 				strikeCall();
-			}
-			if (sunkShips.length == 2) {
-				console.log("You've sunk all my Battleships!!");
-				let playAgain = rls.keyInYN("Would you like to play again?");
-				if (playAgain === true) {
-					checkStrike.length = 0;
-					sunkShips.length = 0;
-					game();
-				} else {
-					console.log("Have a good Day.");
-					process.exit();
-				}
 			}
 		}
 	}
