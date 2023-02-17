7ea3e67ea8c79ea747d1e1bf53093e14738cc21a
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-02-17 15:00:19 -0600

    Made function for grid size

--- part-2.js
+++ part-2.js
@@ -3,5 +3,10 @@ function game() {
 	let ships = [];
 	const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	function buildGrid(size) {
+		let letterCoordinate = letters.slice(0, size);
+		let numCoordinate = nums.slice(0, size);
+	}
+	console.log(buildGrid(10));
 }
 game();
