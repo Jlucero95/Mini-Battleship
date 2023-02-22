825acd97d00958dcab76b60261f96bec1dd60ffb
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-02-22 10:12:39 -0600

    Random generated postion works
    Can randomly select position and place into code given by andrey
    to build out ships.

--- part-2.js
+++ part-2.js
@@ -1,43 +1,37 @@
 const rls = require("readline-sync");
-function game() {
-	let ships = [[], [], [], [], []];
-	let direction = "hor";
-	const twoUnit = 2;
-	const threeUnit = 3;
-	const fourUnit = 4;
-	const fiveUnit = 5;
-	const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-	const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-	function shipPlacement() {
-		let randomLetter = letters
-			.sort(function () {
-				return 0.5 - Math.random();
-			})
-			.slice(0, 9);
-		let randomNum = nums
-			.sort(function () {
-				return 0.5 - Math.random();
-			})
-			.slice(0, 9);
-	}
+let direction = "hor";
+const twoShip = 2;
+const threeShip = 3;
+const fourShip = 4;
+const fiveShip = 5;
+const ship = [];
 
-	let converter = (str, length) => {
-		const x = letters.indexOf(str[0]);
-		const y = str.substring(1) - 1;
-		return [x, y];
-	};
+const placeHolder = [];
+const letters = "abcdefghij".toUpperCase().split("");
 
-	let coord = converter("F5", letters);
+function findLocation() {
+	let randomIndex = Math.floor(Math.random() * letters.length);
+	if (randomIndex > 0) {
+		placeHolder.push(letters[randomIndex - 1] + randomIndex);
+	} else findLocation();
+}
+findLocation();
 
-	if (direction === "hor") {
-		if (coord[1] + fiveUnit < letters.length) {
-			for (let i = 0; i < fiveUnit; i++) {
-				ships[4].push([coord[0], coord[1] + i]);
-			}
+let converter = (str, letters) => {
+	const x = letters.indexOf(str[0]);
+	const y = +str.substring(1) - 1;
+	return [x, y];
+};
+
+let coord = converter(placeHolder[0], letters);
+
+if (direction === "hor") {
+	if (coord[1] + fourShip < letters.length) {
+		for (let i = 0; i < fourShip; i++) {
+			ship.push([coord[0], coord[1] + i]);
 		}
 	}
-
-	console.log(ships);
 }
-game();
+
+console.log(ship);
