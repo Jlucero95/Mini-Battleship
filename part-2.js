47cb81884bd364e827b45900b5b92a981a7fbdf1
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-03-11 14:29:15 -0600

    Storing user inputs work.
    Next step is to find if user input is a hit or a miss.

--- part-2.js
+++ part-2.js
@@ -21,6 +21,7 @@ function grid(num, letter) {
 	return board;
 }
 const game = grid(10, letters);
+console.log(letters);
 
 function placeShip(ship, shipLength) {
 	let randomSection = game[Math.floor(Math.random() * game.length)];
@@ -92,7 +93,7 @@ let checkStrike = [];
 function checkHit() {
 	const indexList = [];
 	for (let i = 0; i < letters.length; i++) {
-		indexList.push(letters.indexOf(letters[i]) + 1);
+		indexList.push((letters.indexOf(letters[i]) + 1).toString());
 	}
 	let strike;
 	while (true) {
@@ -112,6 +113,7 @@ function checkHit() {
 					);
 				} else {
 					checkStrike.push(letter + number);
+					console.log(checkStrike);
 					break;
 				}
 			} else {
